46600a4c459c2e3fd310550642862a7cedde8296
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-10-05 11:33:17 +0300

    Fixed .sh as could

--- make.sh
+++ make.sh
@@ -7,9 +7,11 @@ CXX_FLAGS=$CXX_FLAGS' '-std=c++0x
 
 CXX_COMPILE=g++
 
-$CXX_COMPILE -c src/client/Client.cpp -o build/Client.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
-$CXX_COMPILE -c src/server/Server.cpp -o build/Server.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
-$CXX_COMPILE -c src/logger/Logger.cpp -o build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
+cd src
+$CXX_COMPILE -c client/Client.cpp -o ../build/Client.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
+$CXX_COMPILE -c server/Server.cpp -o ../build/Server.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
+$CXX_COMPILE -c logger/Logger.cpp -o ../build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
+cd ..
 $CXX_COMPILE -o build/log src/test/log.cpp $CXX_FLAGS $INCLUDE_PATH $LIBRARY
 $CXX_COMPILE -o build/client src/test/client.cpp build/Client.o build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
 $CXX_COMPILE -o build/server src/test/server.cpp build/Server.o build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
