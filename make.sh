be1b88d4b28482bfa531d1ba43d8496b1ac854c7
MekaGem <mekagem@gmail.com>
2012-10-07 15:58:57 +0400

    none

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 LIBRARY=$LIBRARY' '-lsfml-system
 LIBRARY=$LIBRARY' '-lsfml-network
 LIBRARY=$LIBRARY' '-lsfml-graphics
+LIBRARY=$LIBRARY' '-lsfml-window
 LIBRARY=$LIBRARY' '-lsigc-2.0
 
 CXX_FLAGS=$CXX_FLAGS' '-Wall
@@ -20,8 +21,10 @@ $CXX_COMPILE -c client/Client.cpp -o ../build/Client.o $CXX_FLAGS $INCLUDE_PATH
 $CXX_COMPILE -c server/Server.cpp -o ../build/Server.o $CXX_FLAGS $INCLUDE_PATH
 $CXX_COMPILE -c logger/Logger.cpp -o ../build/Logger.o $CXX_FLAGS $INCLUDE_PATH
 $CXX_COMPILE -c unit/Unit.cpp -o ../build/Unit.o $CXX_FLAGS $INCLUDE_PATH
+$CXX_COMPILE -c view/View.cpp -o ../build/View.o $CXX_FLAGS $INCLUDE_PATH
 
 $CXX_COMPILE -o ../bin/log test/log.cpp ../build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
 $CXX_COMPILE -o ../bin/client test/client.cpp ../build/Client.o ../build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
 $CXX_COMPILE -o ../bin/server test/server.cpp ../build/Server.o ../build/Logger.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
 $CXX_COMPILE -o ../bin/unit test/unit.cpp ../build/Unit.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
+$CXX_COMPILE -o ../bin/view test/view.cpp ../build/View.o ../build/Unit.o $CXX_FLAGS $INCLUDE_PATH $LIBRARY
--- src/test/unit.cpp
+++ src/test/unit.cpp
@@ -1,7 +1,24 @@
 #include "unit/Unit.hpp"
+#include <SFML/Window.hpp>
 
 int main ()
 {
-	Unit u("hello");
+	sf::RenderWindow window(sf::VideoMode(512, 512), "Title");
+	//window.show();
+
+	junk::PlayerUnit u("hello");
+	u.setPosition(sf::Vector2f(100.0, 100.0));
+	sf::Font font;
+	font.loadFromFile("/Users/mekagem/junk/bin/arial.ttf");
+
+	while (window.isOpen())
+	{
+		window.clear();
+		u.update();
+		window.draw(u);
+		window.draw(sf::Text("Hello", font));
+		window.display();
+	}
+
 	return 0;
 }
