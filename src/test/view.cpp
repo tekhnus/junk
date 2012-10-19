3afee9bde886b181d5947c54e2576658ae54b622
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-10-19 12:02:39 +0400

    started view demo

--- src/test/view.cpp
+++ src/test/view.cpp
@@ -1,18 +1,21 @@
-#include "client/view/Unit.hpp"
+#include "client/view/ClientView.hpp"
+#include "client/controller/ClientController.hpp"
+#include "client/model/ClientModel.hpp"
 #include <SFML/Window.hpp>
 
 int main ()
 {
 	sf::RenderWindow window(sf::VideoMode(512, 512), "Title");
 	//window.show();
-
+	junk::ClientView view;
+	junk::ClientController control;
+	junk::ClientModel model;
 	// junk::View view;
 	junk::PlayerUnit u("Unit", sf::Vector2f(100.0, 100.0), sf::Vector2f(100.0, 100.0));
 	// view.addPlayer(sf::Vector2f(100.0, 100.0), sf::Vector2f(100.0, 100.0));
 
 	// u.setPosition(sf::Vector2f(100.0, 100.0));
-	sf::Font font;
-	font.loadFromFile("/Users/mekagem/junk/bin/arial.ttf");
+	sf::Font font = sf::Font::getDefaultFont();
 
 	while (window.isOpen())
 	{
