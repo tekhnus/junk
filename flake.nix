72a4ccfcc2ac233a4f286d107c4a27faddbc1dbb
zahaaar <zahaaar@avride.ai>
2023-12-25 13:54:41 +0000

    Kinda works?

--- flake.nix
+++ flake.nix
@@ -6,38 +6,46 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              sfgui = import ./sfgui.nix { inherit pkgs; };
-              box2d = import ./box2d.nix { inherit pkgs; };
-            })
-          ];
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.pkg-config
-            # Other dependencies
-          ];
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          (self: super: {
+            sfgui = import ./sfgui.nix { inherit pkgs; };
+            box2d = import ./box2d.nix { inherit pkgs; };
+          })
+        ];
+      };
+    in
+    {
+      packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
+        name = "junk";
+        src = pkgs.lib.cleanSource ./.;
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.pkg-config
+        ];
 
-          buildInputs = [
-            pkgs.stdenv.cc
-            pkgs.boost
-            pkgs.sfml
-            pkgs.mesa
-pkgs.sfgui pkgs.libGLU pkgs.xorg.libX11 pkgs.box2d pkgs.libevent pkgs.thrift ];
+        buildInputs = [
+          pkgs.stdenv.cc
+          pkgs.boost
+          pkgs.sfml
+          pkgs.mesa
+          pkgs.sfgui
+          pkgs.libGLU
+          pkgs.xorg.libX11
+          pkgs.box2d
+          pkgs.libevent
+          pkgs.thrift
+        ];
 
-          cmakeFlags = [
-            "-DBOOST_ROOT=${pkgs.boost}"
-          ];
-        };
-      }
-    );
+        cmakeFlags = [
+          "-DBOOST_ROOT=${pkgs.boost}"
+        ];
+      };
+      # devShell = pkgs.mkShell {
+      #   inputsFrom = [ packages.junk ];
+      # };
+    };
 }
