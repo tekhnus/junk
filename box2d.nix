79e4e8df7967407aa1f7b05d8298388bcfcdb744
Zach Philipchook <z.pilipchuk@gmail.com>
2023-12-29 15:17:07 +0200

    Mac build

--- box2d.nix
+++ box2d.nix
@@ -1,12 +1,21 @@
 # box2d.nix
-{ pkgs ? import <nixpkgs> {}, ... }:
+{ pkgs ? import <nixpkgs> { }, ... }:
 
 pkgs.stdenv.mkDerivation {
   name = "box2d";
   src = ./third_party/box2d;
 
   nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
-  buildInputs = [ pkgs.xorg.libX11 pkgs.xorg.libXrandr pkgs.xorg.libXinerama pkgs.xorg.libXcursor pkgs.xorg.libXi ];
+  buildInputs = [
+    pkgs.xorg.libX11
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXinerama
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXi
+    pkgs.darwin.apple_sdk.frameworks.Carbon
+    pkgs.darwin.apple_sdk.frameworks.Cocoa
+    pkgs.darwin.apple_sdk.frameworks.Kernel
+  ];
 
   buildPhase = ''
     cmake .
--- flake.nix
+++ flake.nix
@@ -1,52 +1,53 @@
 {
-  description = "Your project description";
+  description = "Junk";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [
-          (self: super: {
-            sfgui = import ./sfgui.nix { inherit pkgs; };
-            box2d = import ./box2d.nix { inherit pkgs; };
-          })
-        ];
-      };
-    in
-    rec {
-      packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
-        name = "junk";
-        src = pkgs.lib.cleanSource ./.;
-        nativeBuildInputs = [
-          pkgs.cmake
-          pkgs.pkg-config
-        ];
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              sfgui = import ./sfgui.nix { inherit pkgs; };
+              box2d = import ./box2d.nix { inherit pkgs; };
+            })
+          ];
+        };
+      in
+      rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "junk";
+          src = pkgs.lib.cleanSource ./.;
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.pkg-config
+          ];
 
-        buildInputs = [
-          pkgs.stdenv.cc
-          pkgs.boost
-          pkgs.sfml
-          pkgs.mesa
-          pkgs.sfgui
-          pkgs.libGLU
-          pkgs.xorg.libX11
-          pkgs.box2d
-          pkgs.libevent
-          pkgs.thrift
-        ];
+          buildInputs = [
+            pkgs.stdenv.cc
+            pkgs.boost
+            pkgs.sfml
+            pkgs.mesa
+            pkgs.sfgui
+            pkgs.libGLU
+            pkgs.xorg.libX11
+            pkgs.box2d
+            pkgs.libevent
+            pkgs.thrift
+          ];
 
-        cmakeFlags = [
-          "-DBOOST_ROOT=${pkgs.boost}"
-        ];
-      };
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.lldb ];
-        inputsFrom = [ packages."x86_64-linux".default ];
-      };
-    };
+          cmakeFlags = [
+            "-DBOOST_ROOT=${pkgs.boost}"
+          ];
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.lldb ];
+          inputsFrom = [ packages.default ];
+        };
+      });
 }
