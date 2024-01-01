5255a43e62a321f17d9d8a86bfef1ac4f1bd03f0
Zach Philipchook <z.pilipchuk@gmail.com>
2024-01-01 18:04:29 +0200

    Disable box2d testbed

--- third_party/box2d.nix
+++ third_party/box2d.nix
@@ -6,16 +6,13 @@ pkgs.stdenv.mkDerivation {
   src = ./box2d;
 
   nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
-  buildInputs = [
-    pkgs.xorg.libX11
-    pkgs.xorg.libXrandr
-    pkgs.xorg.libXinerama
-    pkgs.xorg.libXcursor
-    pkgs.xorg.libXi
-  ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-    pkgs.darwin.apple_sdk.frameworks.Carbon
-    pkgs.darwin.apple_sdk.frameworks.Cocoa
-    pkgs.darwin.apple_sdk.frameworks.Kernel
+  buildInputs = [ ];
+
+  cmakeFlags = [
+    "-DBOX2D_INSTALL=ON"
+    "-DBOX2D_BUILD_SHARED=ON"
+    "-DBOX2D_BUILD_EXAMPLES=OFF"
+    "-DBOX2D_BUILD_TESTBED=OFF"
   ];
 
   buildPhase = ''
