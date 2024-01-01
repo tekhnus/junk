b7b2c727501fdac6227d0ee7a5068ce23f7fef5c
Zach Philipchook <z.pilipchuk@gmail.com>
2024-01-01 15:47:19 +0200

    No more pkg-config

--- flake.nix
+++ flake.nix
@@ -26,29 +26,29 @@
           src = pkgs.lib.cleanSource ./.;
           nativeBuildInputs = [
             pkgs.cmake
-            pkgs.pkg-config
             pkgs.ninja
           ];
           buildInputs = [
             pkgs.boost
             pkgs.sfml
-            pkgs.mesa
             pkgs.sfgui
-            pkgs.libGLU
-            pkgs.xorg.libX11
             pkgs.box2d
             pkgs.thrift
+            # The following packages are transitive dependencies.
+            # TODO: investigate why do we need to list them here.
+            pkgs.mesa
+            pkgs.libGLU
+            pkgs.xorg.libX11
           ];
         };
         devShells.default = pkgs.mkShell.override { inherit stdenv; } {
           nativeBuildInputs = [
-            pkgs.clang-tools_17
-            pkgs.fd
-            pkgs.moreutils
-            pkgs.python3
             pkgs.cmake
-            pkgs.pkg-config
             pkgs.ninja
+            pkgs.python3
+            pkgs.clang-tools_17
+            pkgs.moreutils
+            pkgs.fd
           ];
           hardeningDisable = [ "fortify" ];
           inputsFrom = [ packages.default ];
