4de9b9654808ffd10993d9696971386262a1a826
Zach Philipchook <z.pilipchuk@gmail.com>
2024-01-01 14:57:12 +0200

    Remove obsolete targets

--- src/common/utils/Resource.cpp
+++ src/common/utils/Resource.cpp
@@ -1,16 +1,7 @@
 #include "Resource.hpp"
 
-#include <cstdlib>
-#include <iostream>
-
 std::string resource_root = "";
 
 std::string get_resource_path(std::string resource_name) {
-  std::cerr << "Getting resource " << resource_name << std::endl;
-  std::cerr << "(2) Resource root is " << resource_root << std::endl;
-  const char* resource_dir = getenv("JUNK_RESOURCE_DIR");
-  if (resource_dir == nullptr) {
-    resource_dir = resource_root.c_str();
-  }
-  return std::string{resource_dir} + "/" + resource_name;
+  return resource_root + "/" + resource_name;
 }
