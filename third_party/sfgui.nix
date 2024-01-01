0b6cbfa09e6ea6f244b1384b911c194895fee4e2
zahaaar <zahaaar@avride.ai>
2024-01-01 16:47:09 +0000

    Somehow fix SFGUI on linux

--- third_party/sfgui.nix
+++ third_party/sfgui.nix
@@ -8,12 +8,19 @@ pkgs.stdenv.mkDerivation {
   nativeBuildInputs = [ pkgs.cmake ];
   buildInputs = [
     pkgs.sfml
-  ] ++ pkgs.lib.optionals
+  ]
+  # SFGUI uses SFML/OpenGL.h, so it must
+  # link OpenGL itself.
+  ++ pkgs.lib.optionals
     pkgs.stdenv.isDarwin
     [
-      # SFGUI uses SFML/OpenGL.h, so it must
-      # link OpenGL itself.
       pkgs.darwin.apple_sdk.frameworks.OpenGL
+    ]
+  ++ pkgs.lib.optionals
+    pkgs.stdenv.isLinux
+    [
+      pkgs.libGL
+      pkgs.xorg.libX11
     ];
 
   cmakeFlags = [
